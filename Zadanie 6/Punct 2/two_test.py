a95e0f0efa885f83b519b85da3434d7aa825eabc
shmigovnyak <160512113+shmigovnyak@users.noreply.github.com>
2024-05-07 22:04:32 +0300

    добавила 6 задание

--- Zadanie 6/Punct 2/two_test.py
+++ Zadanie 6/Punct 2/two_test.py
@@ -1,10 +1,10 @@
 from merge_and_write import merge_and_write
 import pytest
 @pytest.mark.parametrize(
-        "file1, file2, output, result",
+        "file_1, file_2, file_3, shmiga",
         [
             ("./3.txt","./12.txt","./out.txt","Один из файлов не найден"),
         ]
 )
-def test_merge_and_write(file1, file2, output, result):
-    assert merge_and_write(file1, file2, output) == result
+def test_merge_and_write(file_1, file_2, file_3, shmiga):
+    assert merge_and_write(file_1, file_2, file_3) == shmiga
